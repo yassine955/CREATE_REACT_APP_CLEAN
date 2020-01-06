f056513fa72f041591b4065df2de749f954ed0d9
Yassine <54895428+yassine955@users.noreply.github.com>
2020-01-06 14:37:29 +0100

    init 1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-clean-up
+
+This package has a cleaned up version of the newest react version
+
+## Usage
+
+```python
+npm i and then npm start
+```
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.scss';
+
+const App = () => (
+  <h1 className={classes.welcome}>
+    <span role='img' aria-label='emoji'>
+      No clean up anymore 😒
+    </span>
+  </h1>
+);
+
+export default App;
